f32fe8b9ea4d202ec46f4d2e0e0462a835fa6efb
yamaa <yamaa.com987654321@outlook.jp>
2025-05-31 10:59:04 +0000

    run.py -> main.pyに移動

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,29 @@
 import asyncio
+import json
+from pathlib import Path
+from typing import Any
 
-from run import run
+from langchain_mcp_adapters.client import MultiServerMCPClient  # type: ignore
+
+from app import App, load_config
+from server import Server
 
 
-def main() -> None:
-    # MCPアダプタが非同期呼び出しにしか対応していないのでrun.pyを用意
-    asyncio.run(run())
+def _load_mcp_config_file(mcp_config_path: str) -> dict[str, Any]:
+    """data/mcp/config.jsonからmcpクライアントを返す."""
+    path = Path(mcp_config_path)
+    with path.open("r", encoding="utf-8") as text:
+        return json.load(text)
+
+
+async def main() -> None:
+    params = _load_mcp_config_file("data/conf/mcp_config.json")
+    async with MultiServerMCPClient(params["mcpServers"]) as client:
+        app_config = await load_config(client)
+        app = App(app_config)
+        server = Server(app)
+        await server.listen_and_serve(8080)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
